7b0888b3699584947b60f07a327781e6c098922f
pvrajeshwari <rajeshwari.xworks@gmail.com>
2023-06-21 10:29:16 +0530

    array-code

---
+++ Array.java
@@ -0,0 +1,113 @@
+ class Array{
+	public static void main(String[] args){
+	int[] noOfClass = {4,5,3,9,8,7};
+	int[] salarys = {500000,65000,787600,45000};
+	int[] priceOfMobiles = {10000,15000,17000,19000,20000};
+	char[] grade ={'a','b','c','d'};
+	char[] vowels = {'a','e','i','o','u'};
+	char[] gender = {'M','F'};
+	float[] sgpa = {8.1f,8.4f,7.5f,9.1f,7.0f};
+	float[] heigth = {5.3f,4.3f,6.0f,5.32f};
+	String[] name ={"rajeshwari","yash","pooja","anjali"};
+	String[] place ={"hubli","bengaluru"};
+	System.out.println(noOfClass[6]);
+	System.out.println(salarys[4]);
+	System.out.println(priceOfMobiles[5]);
+	System.out.println(grade[4]);
+	System.out.println(vowels[3]);
+	System.out.println(sgpa[1]);
+	System.out.println(gender[1]);
+	System.out.println(heigth[4]);
+	System.out.println(name[4]);
+	System.out.println(place[2]);
+	int[] roomNumber =new int[5];
+	roomNumber[0] = 101;
+	roomNumber[1] = 102;
+	roomNumber[2] = 103;
+	roomNumber[3] = 104;
+	roomNumber[4] = 105;
+	int[] windows = new int[5];
+	windows[0] = 7;
+	windows[1] = 8;
+	windows[2] = 9;
+	windows[3] = 10;
+	windows[4] = 11;
+	int[] areaCrossNo = new int[5];
+	areaCrossNo[0] = 1;
+	areaCrossNo[1] = 2;
+	areaCrossNo[2] = 3;
+	areaCrossNo[3] = 4;
+	areaCrossNo[4] = 5;
+	char[] rainbowColors = new char[7];
+	rainbowColors[0] = 'r';
+	rainbowColors[1] = 'o';
+	rainbowColors[2] = 'y';
+	rainbowColors[3] = 'g';
+	rainbowColors[4] = 'b';
+	rainbowColors[5] = 'i';
+	rainbowColors[6] = 'v';
+	char[] subjects = new char[5];
+	subjects[0] = 'e';
+	subjects[1] = 'k';
+	subjects[2] = 'h';
+	subjects[3] = 'm';
+	subjects[4] = 's';
+	char[] colorOfTree = new char[5];
+	colorOfTree[0] = 'g';
+	colorOfTree[1] = 'r';
+	colorOfTree[2] = 'e';
+	colorOfTree[3] = 'e';
+	colorOfTree[4] = 'n';
+	float[] age = new float[5];
+	age[0] = 10.5f;
+	age[1] = 12.5f;
+	age[2] = 11.5f;
+	age[3] = 1.5f;
+	age[4] = 17.5f;
+	float[] cgpa = new float[5];
+	cgpa[0] = 7.8f;
+	cgpa[1] = 8.3f;
+	cgpa[2] = 9.5f;
+	cgpa[3] = 5.8f;
+	cgpa[4] = 6.2f;
+	float[] manWeigth = new float[5];
+	manWeigth[0] = 20.5f;
+	manWeigth[1] = 10.5f;
+	manWeigth[2] = 11.5f;
+	manWeigth[3] = 15.5f;
+	manWeigth[4] = 22.8f;
+
+	for(int i=0;i<roomNumber.length;i++){
+		System.out.println(roomNumber[i]);
+	}
+	for(int i=0;i<manWeigth.length;i++)
+	{
+		System.out.println(manWeigth[i]);
+	}
+	for(int i=0;i<cgpa.length;i++)
+	{
+	System.out.println(cgpa[i]);
+	}
+	for(int i=0;i<age.length;i++)
+	{
+	System.out.println(age[i]);
+	}
+	for(int i=0;i<colorOfTree.length;i++)
+	{
+	System.out.println(colorOfTree);
+	}
+	System.out.println(windows);
+	System.out.println(rainbowColors);
+	System.out.println(subjects);
+	System.out.println(areaCrossNo);
+	
+	
+
+	
+	}
+	
+
+
+
+
+}
