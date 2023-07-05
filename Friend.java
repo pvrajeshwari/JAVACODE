393dc9403693abc23b36e038b768242ca87f0bd5
pvrajeshwari <rajeshwari.xworks@gmail.com>
2023-07-05 10:06:54 +0530

    String method

---
+++ Friend.java
@@ -0,0 +1,72 @@
+public class Friend{
+	public static void main (String[] args){
+		String studentName ="rajeshwari";
+		String carBrand ="BMW";
+		String placeName ="hubli";
+		String color ="blue";
+		String clgTime ="6 hr";
+		String phonebrand ="MI";
+		String usnOfStudent ="2ka19is029";
+		String clgName ="SKSVMACET";
+		String rollNo ="09";
+		String roomNo ="101";
+
+
+		String name = new String("madhu");
+		String pinCode = new String("581113");
+		String hrName = new String("bvpatil");
+		String studentFatherName =new String("veeranagoud");
+		String friendName = new String ("Shreya");
+		String currentbal = new String("1202");
+		String instituteNmae = new String("X-workZ");
+		String bookNumber = new String("0293");
+		String trainNo =new String("10122");
+		String seatColor = new String("red");
+
+
+
+		String returne= studentName.substring(0);
+	    System.out.println(returne);
+	    boolean type=clgName.endsWith("t");
+	    System.out.println(type);
+	    int valued=rollNo.indexOf(2);
+	    System.out.println(valued);
+	    String returenedValue= friendName.toUpperCase();
+	    System.out.println(returenedValue);
+	    String returenedname= clgName.toLowerCase();
+	    System.out.println(returenedname);
+	    String realedValue= color.replace('b','e');
+	    System.out.println(realedValue);
+	    System.out.println(pinCode);
+	    System.out.println(name); 
+        System.out.println(usnOfStudent);
+        System.out.println(bookNumber);
+        System.out.println(carBrand);
+        System.out.println(seatColor); 
+        System.out.println(trainNo); 
+        System.out.println(hrName); 
+        System.out.println(placeName); 
+        System.out.println(instituteNmae);
+        System.out.println(studentFatherName);
+        System.out.println(currentbal); 
+        System.out.println(roomNo); 
+        System.out.println(phonebrand); 
+
+
+ 
+ 
+ 
+ 
+
+
+
+
+ 
+ 
+
+	
+	
+
+
+	}
+}
